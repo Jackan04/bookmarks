0e9f3a1faf67b5b07d408e1e9c49b8b6eb485901
jackan04 <jacob.asker@icloud.com>
2026-04-05 18:02:40 +0200

    feat: implement user registration

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,9 +1,32 @@
 import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma";
+import { signToken } from "../lib/jwt";
 
 export async function register(
   req: Request,
   res: Response,
   next: NextFunction,
-) {}
+) {
+  try {
+    const { username, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        password: passwordHash,
+      },
+    });
+
+    const token = signToken(user.id);
+    res.status(201).json({ token });
+  } catch (error) {
+    return next(error);
+  }
+}
+
 export async function login(req: Request, res: Response, next: NextFunction) {}
+
 export async function getMe(req: Request, res: Response, next: NextFunction) {}
