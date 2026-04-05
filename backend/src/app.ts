67c3ddd3113bdff6bb210439e12d82ae64495520
jackan04 <jacob.asker@icloud.com>
2026-04-05 14:44:39 +0200

    refactor: remove server listening logic from app.ts and export app instance

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,4 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+export default app;
