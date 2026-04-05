ee5a2936651ce5fb5c8457faa894e35a6c1c1e82
jackan04 <jacob.asker@icloud.com>
2026-04-05 18:33:16 +0200

    feat: add authLimiter and authenticate middleware to auth routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { register, login, getMe } from "../controllers/authController";
+import { authLimiter, authenticate } from "../utils/authentication";
 
 const authRouter = Router();
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.post("/me", getMe);
+authRouter.post("/register", authLimiter, register);
+authRouter.post("/login", authLimiter, login);
+authRouter.post("/me", authenticate, getMe);
 
 export default authRouter;
