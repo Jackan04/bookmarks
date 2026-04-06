85428cd16cdfa78a81ea9fb27031d800000d17a9
jackan04 <jacob.asker@icloud.com>
2026-04-06 11:16:11 +0200

    feat: add /bookmarks router

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRoutes";
 import userRouter from "./userRoutes";
+import bookmarkRouter from "./bookmarkRoutes";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
+router.use("/bookmarks", bookmarkRouter);
 
 export default router;
