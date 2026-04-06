b28471cf61b3f4247fcb1b77a25b9521c2062717
jackan04 <jacob.asker@icloud.com>
2026-04-06 13:24:53 +0200

    feat: add authentication middleware to user and bookmark routes

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import authRouter from "./authRoutes";
 import userRouter from "./userRoutes";
 import bookmarkRouter from "./bookmarkRoutes";
+import { authenticate } from "../middleware/authentication";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/users", userRouter);
-router.use("/bookmarks", bookmarkRouter);
+router.use("/users", authenticate, userRouter);
+router.use("/bookmarks", authenticate, bookmarkRouter);
 
 export default router;
